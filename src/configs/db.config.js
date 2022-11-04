13ca6f1262ad579cb6483c8c75b04019356aab79
Sebastian Moyano <90289493+SebaIMR@users.noreply.github.com>
2022-11-04 17:25:11 -0300

    First step database

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,1 +1,17 @@
-xd
+const { Client } = require('pg')
+const client = new Client()
+/*
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+client.connect()
+ingresar los datos de nuestra data base
+**/
+await client.connect()
+const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+console.log(res.rows[0].message) // Hello world!
+await client.end()
