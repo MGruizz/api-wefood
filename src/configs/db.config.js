bb7ee58ac7e54bbce565523546955027716d9aea
Sebastian Moyano <90289493+SebaIMR@users.noreply.github.com>
2022-11-05 17:44:51 -0300

    Pool for client

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,11 +1,19 @@
-const {Client} = require('pg')
+const {Pool,Client} = require('pg')
 
-const client = new Client({
+// const client = new Client({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "123",
+//   database: "Wefood"
+// })
+
+const pool = new Pool({
   host: "localhost",
   user: "postgres",
   port: 5432,
   password: "123",
-  database: "Respaldo"
+  database: "Wefood"
 })
 
-module.exports = client
+module.exports = pool
