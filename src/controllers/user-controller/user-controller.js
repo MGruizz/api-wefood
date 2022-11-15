93c2290e47f4bdee3efaf30c7722d5b7ae3235c1
beiache <makbeiache@gmail.com>
2022-11-15 18:50:30 -0300

    Hash de passwords, login y creacion-usuario

--- src/controllers/user-controller/user-controller.js
+++ src/controllers/user-controller/user-controller.js
@@ -1,5 +1,8 @@
 const pool = require('../../configs/db.config')
 
+const bcryptjs = require("bcryptjs");
+
+
 const getUsersById = async (req,res) => {
     const id = req.params.id;
     const response = await pool.query(`SELECT * FROM usuarios where idusuario = ${id}`);
@@ -7,6 +10,68 @@ const getUsersById = async (req,res) => {
     pool.end;
 }
 
+const logearUsuario = async (req, res) => {
+    const {correoElectronico, password} = req.body;
+    console.log(correoElectronico, password);
+    const user = await pool.query(
+        'SELECT * FROM usuarios where correoelectronico = $1', [correoElectronico],
+        (err, res) => {
+            if (err) {
+                res.status(401).send(console.log(err.stack));
+            } else {
+                console.log(res.rows[0])
+            }
+
+            if(results.rows.length > 0) {
+                const user = results.rows[0];
+
+                bcryptjs.compare(password, user.password, (err, isMatch) => {
+                    if(err) {
+                        res.status(401).send(console.log(err.stack));
+                    }
+
+                    if(isMatch) {
+                        res.status(200).json(user);
+                    }
+                });
+            }
+        }
+    );
+}
+
+const registrarUsuario = async (req, res) => {
+    const {nombrepersona, correoelectronico, password} = req.body;
+    let hashPassword = await bcryptjs.hash(password, 10);
+    pool.query(
+        'SELECT * FROM usuarios where correoelectronico = $1', [correoelectronico],
+        (err, results) => {
+            if(err) {
+                res.status(401).send(console.log(err.stack));
+            }
+            console.log(results.rows[0]);
+
+            if(results.rows.length > 0) {
+                res.status(401).send(console.log(err.stack));
+            } else {
+                console.log(nombrepersona, correoelectronico, password, hashPassword, 'holas');
+                pool.query(
+                    `INSERT INTO usuarios (nombrepersona, correoelectronico, password)
+                    VALUES ($1, $2, $3)`, [nombrepersona, correoelectronico, hashPassword],
+                    (err, results) => {
+                        if(err) {
+                            res.status(401).send(console.log(err.stack));
+                        } else {
+                            res.status(200).send(results);
+                        }
+                    }
+                )
+            }
+        }
+    );
+}
+
 module.exports = {
-    getUsersById
+    getUsersById,
+    logearUsuario,
+    registrarUsuario
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const {Router} = require ('express');
 const router = Router();
 
 const {getUsersById} = require('../controllers/user-controller/user-controller')
+const {logearUsuario} = require('../controllers/user-controller/user-controller')
+const {registrarUsuario} = require('../controllers/user-controller/user-controller')
 const {getAllRecipes} = require('../controllers/recipe-controller/recipe-controller')
 const {getRecipesByUserId} = require('../controllers/recipe-controller/recipe-controller')
 const {getTagsByRecipeID} = require('../controllers/tag-controller/tag-controller')
@@ -9,6 +11,8 @@ const {getAllTags} = require('../controllers/tag-controller/tag-controller')
 
 //Ruta usuario
 router.get('/usuarios/:id/',getUsersById);
+router.post('/login/', logearUsuario);
+router.get('/creacion-usuario/', registrarUsuario)
 //Ruta Receta
 router.get('/recetas/',getAllRecipes);
 router.get('/recetas/:id',getRecipesByUserId)
